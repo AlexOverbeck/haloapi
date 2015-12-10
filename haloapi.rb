e24a886060d1b0291989ad520829a902a0c49cf6
Alex Overbeck <alexoverbeck@gmail.com>
2015-12-10 13:24:57 -0500

    Initial halo api setup.

---
+++ haloapi.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require 'net/http'
+require 'json'
+
+get '/' do
+  content_type :json
+  player = 'TheInternets'
+  key = '1f81e269a1c54e46a43ce766f35eccb0'
+  uri = URI("https://www.haloapi.com/stats/h5/players/#{player}/matches")
+
+  uri.query = URI.encode_www_form({
+    count: '1'
+  })
+
+  request = Net::HTTP::Get.new(uri.request_uri)
+  request['Ocp-Apim-Subscription-Key'] = key
+
+  response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+    http.request(request)
+  end
+
+  data = JSON.parse(response.body)
+
+  data['Results'].to_json
+end
